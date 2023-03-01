90ee4d2c2747e00675e1bc483750ab15bdb39000
David <43140185+exhaled@users.noreply.github.com>
2023-03-01 18:08:59 +0100

    Documentation

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
-// Nacitame class
+// Načítame class
 include('libs/mfcr.class.php');
 $mfcr = new MFCR();
 
-// Ziskame udaje o subjekte podla ICO
-// Input funkcie musi byt string
-$udaje = $mfcr->udaje_subjektu('71214011');
+// Získame údaje o subjekte podľa IČO
+// Input funkcie musí byt string
+$udaje = $mfcr->info_subjektu('71214011');
 
-// Akcia funkcia vrati false znamena to ze subjekt nexistuje
+// Akcia funkcia vráti false znamená to, že subjekt neexistuje
 if ($udaje) {
     echo 'Názov firmy: ' . $udaje->nazov . PHP_EOL;
     echo 'Typ firmy: ' . $udaje->typ . PHP_EOL;
--- libs/mfcr.class.php
+++ libs/mfcr.class.php
@@ -3,35 +3,41 @@ class MFCR {
     private $api_endpoint;
 
     public function __construct() {
-        // Endpoint z ktoreho ziskame data
+        // Endpoint z ktorého získame data
         $this->api_endpoint = 'https://wwwinfo.mfcr.cz/cgi-bin/ares/darv_bas.cgi?ico=';
     }
 
-    public function udaje_subjektu(string $ico) {
-        // Ziskame data z endpointu
+    /**
+     * Získanie informácie z českého registra firiem.
+     *
+     * @param string $ico IČO subjektu
+     * @return object|false Ak IČO je platné a subjekt sa nachádza v registri, vráti objekt, ak nie vráti hodnotu false.
+     */
+    public function info_subjektu(string $ico) {
+        // Získame dáta z endpointu
         $xml_data = file_get_contents($this->api_endpoint.$ico);
 
-        // Nahradime znaky aby XML bol vo spravnom formate
+        // Nahradíme znaky, aby bolo XML vo správnom formáte
         $xml_str = str_replace(PHP_EOL, '', $xml_data);
 
-        // Nacitame XML do premenej
+        // Načítame XML do premennej
         $xml = simplexml_load_string($xml_str, 'SimpleXMLElement', LIBXML_NOCDATA);
 
-        // Ziskame xml prefixy a zaregistrujeme ich ako XPath
+        // Získame XML prefixy a zaregistrujeme ich ako XPath
         foreach ($xml->getDocNamespaces(true) as $prefix => $i) {
             $xml->registerXPathNamespace($prefix, $i);
         }
 
-        // Ziskame udaje cez XPath
+        // Získame údaje pomocou XPath
         $adresa = $xml->xpath('//D:AD/D:UC | //D:AD/D:PB');
 
-        // Ak adresa je prazdna znamena to ze subjekt neexistuje
+        // Ak adresa je prázdna, znamená to že subjekt neexistuje
         if (!empty($adresa)) {
             $nazov = $xml->xpath('//D:VBAS/D:OF');
             $typ = $xml->xpath('//D:VBAS/D:PF/D:NPF');
             $zamestnanci = $xml->xpath('//D:VBAS/D:KPP');
 
-            // Vytvorime objekt
+            // Vytvoríme objekt
             return (object) [
                 'adresa' => $adresa[0] .', '. $adresa[1],
                 'nazov' => (string)$nazov[0],
@@ -39,6 +45,7 @@ class MFCR {
                 'zamestnanci' => (string)$zamestnanci[0]
             ];
         } else {
+            // Ak subjekt neexistuje vrátime hodnotu false
             return false;
         }
     }
